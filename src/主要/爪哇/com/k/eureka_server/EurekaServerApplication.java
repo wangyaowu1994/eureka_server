59428f68720bfcc4c6dd2748c90a34a874cd5655
wangyaowu1994 <68881697+wangyaowu1994@users.noreply.github.com>
2020-07-28 10:25:31 +0800

    Update and rename src/main/java/com/cykj/eureka_server/EurekaServerApplication.java to src/主要/爪哇/com/k/eureka_server/EurekaServerApplication.java

--- src/主要/爪哇/com/k/eureka_server/EurekaServerApplication.java
+++ src/主要/爪哇/com/k/eureka_server/EurekaServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServerApplication {
 
+    //姓名更新
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
